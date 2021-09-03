dc33d0da34ae828d985baecea01b2d7f6f998de9
Stari <darkomitrovski@rocketmail.com>
2021-09-03 12:16:12 +0200

    upload ucenje

---
+++ homework/nodeserver.js
@@ -0,0 +1,5 @@
+const http = require('http');
+const server = http.createServer();
+server.listen(8080,() => {
+    console.log('Domasno;');
+})
---
+++ homework/objects.js
@@ -0,0 +1,15 @@
+const objectOne = {}
+
+ const objectWithProperties = {
+     propertyOne: 5,
+     propertyTwo: 8,
+     propertyThree:13,
+     propertyFour: false,
+     propertyFive: 'Darko',
+     propertySix: () => {
+         console.log('Something')
+     }
+ }
+ 
+ objectWithProperties.propertyFour()
+ objectWithProperties.propertyOne
